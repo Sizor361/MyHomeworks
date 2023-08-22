234e225c4253e4c57a74570d1eda3cad838252a4
sizor <sizor@LAPTOP-JBROU0NO>
2023-08-22 14:59:27 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Full Name";
+            byte age = 0;
+            string email = "Your email";
+            float scoresIt = 0f;
+            float scoresMath = 0f;
+            float scoresPhys = 0f;
+
+            Console.WriteLine($"Full name: {fullName}\n" +
+                $"Age: {age}\n" +
+                $"Email: {email}\n" +   
+                $"IT scores: {scoresIt}\n" +
+                $"Mathematics scores: {scoresMath}\n" +
+                $"Physics scores: {scoresPhys}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float scores;
+            int result;
+
+            scores = (5 + 4 + 5 + 2 + 4 + 3 + 4 + 2 + 2) / 9f;
+
+            if (scores - (int)scores >= 0.5 && scores - (int)scores != 0) //условие на округление оценки в большую сторону при 0.5 остатке и выше
+            {
+                scores++;
+            }
+
+            result = (int)scores;
+            Console.WriteLine("Ученик получил следующие оценки: 5, 4, 5, 2, 4, 3, 4, 2, 2");
+            Console.ReadKey();
+            Console.Write($"Его итоговая оценка - {result}");
+            Console.ReadKey();
+        }
+    }
+}
