0ba1b71ad87432caf5029a1a0a8d138654029374
Влад Рязанцев <picachu361raichu@gmail.com>
2023-08-22 20:38:26 +0400

    Изменил переменные, переместил среднее арифметическое, убрал округление в большую сторону

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -11,19 +11,14 @@ namespace Task2
         static void Main(string[] args)
         {
             float scores;
-            int result;
+            int avg;
 
-            scores = (5 + 4 + 5 + 2 + 4 + 3 + 4 + 2 + 2) / 9f;
+            scores = 5 + 4 + 5 + 2 + 4 + 3 + 4 + 2 + 2;
+            avg = (int)scores / 9;
 
-            if (scores - (int)scores >= 0.5 && scores - (int)scores != 0) //условие на округление оценки в большую сторону при 0.5 остатке и выше
-            {
-                scores++;
-            }
-
-            result = (int)scores;
             Console.WriteLine("Ученик получил следующие оценки: 5, 4, 5, 2, 4, 3, 4, 2, 2");
             Console.ReadKey();
-            Console.Write($"Его итоговая оценка - {result}");
+            Console.Write($"Его итоговая оценка - {avg}");
             Console.ReadKey();
         }
     }
